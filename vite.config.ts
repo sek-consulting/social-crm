dd27e4d1c80fa8056248816d4cd20468415d549e
Stefan E-K <stefan.eideloth@gmail.com>
2023-05-09 10:49:21 +0200

    configs prettified

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
-import prpc from "@prpc/vite";import path from "path";
-  
+import path from "path"
+
+import prpc from "@prpc/vite"
+import solid from "solid-start/vite"
+import { defineConfig } from "vite"
+
 export default defineConfig(() => {
   return {
     plugins: [prpc(), solid({ ssr: true })],
     ssr: { external: ["@prisma/client"] },
     resolve: {
       alias: {
-        rpc: path.join(__dirname, "src", "server", "api"),
-      },
-    },
-  };
-});
-  
+        rpc: path.join(__dirname, "src", "server", "api")
+      }
+    }
+  }
+})
