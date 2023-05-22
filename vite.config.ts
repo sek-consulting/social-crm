59afe26471e229b5bf4eb36e91f92f3ac365354e
Stefan E-K <stefan.eideloth@gmail.com>
2023-05-22 22:47:53 +0200

    vercel deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import path from "path"
 
+import vercel from "solid-start-vercel"
+
 import prpc from "@prpc/vite"
 import solid from "solid-start/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig(() => {
   return {
-    plugins: [prpc(), solid({ ssr: true })],
+    plugins: [prpc(), solid({ ssr: true, adapter: vercel({}) })],
     ssr: { external: ["@prisma/client"] },
     resolve: {
       alias: {
