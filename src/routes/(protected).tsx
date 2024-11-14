cfac9deb4e92d6863bc5c5100a70c510fbe43da3
Stefan E-K <stefan.eideloth@gmail.com>
2024-11-14 08:50:50 +0100

    removed automated breadcrumbs

--- src/routes/(protected).tsx
+++ src/routes/(protected).tsx
@@ -1,22 +1,14 @@
-import { createMemo, Index, Show, Suspense, type ParentProps } from "solid-js"
+import { Suspense, type ParentProps } from "solid-js"
 import type { RouteDefinition } from "@solidjs/router"
-import { createAsync, useLocation } from "@solidjs/router"
+import { createAsync } from "@solidjs/router"
 
 import { AppSidebar } from "~/components/app-sidebar"
 import { ModeToggle } from "~/components/mode-toggle"
 import SearchBar from "~/components/search-bar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbSeparator
-} from "~/components/ui/breadcrumb"
-import { Separator } from "~/components/ui/separator"
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar"
 import { UserNav } from "~/components/user-nav"
 import { AuthProvider } from "~/hooks/use-auth"
 import { getUser } from "~/lib/auth"
-import { cn } from "~/lib/utils"
 
 export const route: RouteDefinition = {
   preload() {
@@ -24,24 +16,8 @@ export const route: RouteDefinition = {
   }
 }
 
-function split(path: string) {
-  if (!path) return []
-  return path
-    .split("/")
-    .filter((segment) => segment !== "")
-    .map((segment) =>
-      segment
-        .split("-")
-        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(" ")
-    )
-}
-
 export default function ProtectedLayout(props: ParentProps) {
   const user = createAsync(() => getUser(), { deferStream: true })
-
-  const location = useLocation()
-  const pathList = createMemo(() => split(location.pathname))
 
   return (
     <Suspense fallback="loading...">
@@ -52,25 +28,6 @@ export default function ProtectedLayout(props: ParentProps) {
             <header class="flex h-12 shrink-0 items-center justify-between gap-2 border-b px-2">
               <div class="flex items-center gap-2">
                 <SidebarTrigger class="size-8" />
-                <Separator orientation="vertical" class="mr-2 h-4" />
-                <Breadcrumb class="hidden md:block">
-                  <BreadcrumbList>
-                    <Index each={pathList()}>
-                      {(path, idx) => (
-                        <>
-                          <Show when={idx > 0}>
-                            <BreadcrumbSeparator />
-                          </Show>
-                          <BreadcrumbItem
-                            class={cn(idx === pathList().length - 1 && "text-foreground")}
-                          >
-                            {path()}
-                          </BreadcrumbItem>
-                        </>
-                      )}
-                    </Index>
-                  </BreadcrumbList>
-                </Breadcrumb>
               </div>
               <div class="flex items-center gap-2">
                 <SearchBar />
